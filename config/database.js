319bdb4495267ee31e0cc87690f515a3af7bd27a
Yogesh Byanjankar <yogesh.benz@yahoo.com>
2019-09-02 22:40:48 -0500

    login

---
+++ config/database.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+    'connection':{
+        'host':'localhost',
+        'user':'root',
+        'password':''
+    },
+    'database':'calendar',
+    'user_table':'login'
+   }
