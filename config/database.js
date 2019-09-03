3f6c162457a9f53e29d552e7d1b6d68e0c1a324f
brittanyboren <54384605+brittanyboren@users.noreply.github.com>
2019-09-02 23:45:59 -0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -3,7 +3,8 @@ module.exports = {
     'connection':{
         'host':'calendar.cv13xfx0hgdu.us-west-2.rds.amazonaws.com',
         'user':'admin',
-        'password':'Fall2019TeamRed'
+        'password':'Fall2019TeamRed',
+        'timeout':60000
     },
     'database':'calendar',
     'user_table':'login'
