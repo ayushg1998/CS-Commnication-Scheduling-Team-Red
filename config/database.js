8327e1236a7ded42e2fa69053b3f5a07a65f97b6
Yogesh Byanjankar <yogesh.benz@yahoo.com>
2019-09-24 01:57:50 -0500

    login,create-student/faculty, and other changes

--- config/database.js
+++ config/database.js
@@ -1,11 +1,22 @@
 
-module.exports = {
+// module.exports = {
+//     'connection':{
+//         'host':'calendar.clni1vzorkk4.us-east-2.rds.amazonaws.com',
+//         'user':'admin',
+//         'password':'Fall2019TeamRed',
+//         'port':'3306'
+//     },
+//     'database':'calendar',
+//     'user_table':'login'
+//    }
+
+module.exports=
+{
     'connection':{
-        'host':'calendar.clni1vzorkk4.us-east-2.rds.amazonaws.com',
-        'user':'admin',
-        'password':'Fall2019TeamRed',
-        'port':'3306'
+        'host':'localhost',
+        'user':'root',
+        'password':''
     },
     'database':'calendar',
     'user_table':'login'
-   }
+};
