0042f3cff3e2628f977df258fd588408b15b6e91
DavidLemons <davidlemons83@gmail.com>
2019-10-16 22:16:00 -0500

    condition render added

--- react-front/src/components/Navigation.js
+++ react-front/src/components/Navigation.js
@@ -47,11 +47,66 @@ class NavDropdown extends React.Component {
   }
 }
 
-
 class AuthenticatedNavigation extends React.Component {
   render() {
-    return (
-      <nav className="navbar navbar-expand-lg navbar-custom navbar-dark">
+        
+    let user =localStorage.getItem('user');
+    let userArray = user.split(','); 
+           for(let i =0; i<userArray.length; i++){
+                if(userArray[i].includes("userType"))
+                {
+                  if(userArray[i].includes("faculty"))
+                  {
+                    console.log(userArray[i]);
+                    return (
+      
+                      <nav className="navbar navbar-expand-lg navbar-custom navbar-dark">
+                        <a className="navbar-brand" href="/">CS Communication System</a>
+                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                          <span className="navbar-toggler-icon"></span>
+                        </button>
+                
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                          <ul className="navbar-nav mr-auto">            
+                            <NavItem path="/dashboard" name="Dashboard" />
+                
+                            <NavDropdown name="Calendar">
+                                <a className="dropdown-item" href="/notfound">Share</a>
+                                <a className="dropdown-item" href="/notfound">Export</a>
+                                <a className="dropdown-item" href="/notfound">Edit</a>
+                            </NavDropdown>
+                
+                            <NavDropdown name="Messages">
+                                <a className="dropdown-item" href="/notfound">View Messages</a>
+                            </NavDropdown>
+                
+                            <NavDropdown name="Events">
+                                <a className="dropdown-item" href="/createEvent">Create Event</a>
+                            </NavDropdown>
+                
+                            <NavDropdown name="Appointment">
+                                <a className="dropdown-item" href="/Appointment">Make Appointment Slots</a>
+                            </NavDropdown>
+                
+                            <NavDropdown name="Groups">
+                                <a className="dropdown-item" href="/notfound">View Current Groups</a>
+                                <a className="dropdown-item" href="/notfound">Create New Group</a>
+                            </NavDropdown>
+                            
+                          </ul>
+                          <ul className="navbar-nav ml-auto">
+                              <NavItem  path="/logout" name="Logout" />            
+                          </ul>
+                        </div>
+                      </nav> 
+                    )
+                  }
+                  if(userArray[i].includes("admin"))
+                  {
+                    console.log(userArray[i]);
+                    return (
+      
+                      <nav className="navbar navbar-expand-lg navbar-custom navbar-dark">
         <a className="navbar-brand" href="/">CS Communication System</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
@@ -77,7 +132,6 @@ class AuthenticatedNavigation extends React.Component {
 
             <NavDropdown name="Appointment">
                 <a className="dropdown-item" href="/Appointment">Make Appointment Slots</a>
-                <a className="dropdown-item" href="/MakeAppointment">Sign Up For an Appointment</a>
             </NavDropdown>
 
             <NavDropdown name="Groups">
@@ -90,8 +144,59 @@ class AuthenticatedNavigation extends React.Component {
               <NavItem  path="/logout" name="Logout" />            
           </ul>
         </div>
-      </nav>
-    )
+      </nav> 
+                    )
+
+                  }
+                  if(userArray[i].includes("student"))
+                  {
+                    console.log(userArray[i]);
+                    return (
+      
+                      <nav className="navbar navbar-expand-lg navbar-custom navbar-dark">
+        <a className="navbar-brand" href="/">CS Communication System</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">            
+            <NavItem path="/dashboard" name="Dashboard" />
+
+            <NavDropdown name="Calendar">
+                <a className="dropdown-item" href="/notfound">Share</a>
+                <a className="dropdown-item" href="/notfound">Export</a>
+                <a className="dropdown-item" href="/notfound">Edit</a>
+            </NavDropdown>
+
+            <NavDropdown name="Messages">
+                <a className="dropdown-item" href="/notfound">View Messages</a>
+            </NavDropdown>
+
+            <NavDropdown name="Events">
+                <a className="dropdown-item" href="/createEvent">Event Sign Up</a>
+            </NavDropdown>
+
+            <NavDropdown name="Appointment">
+                <a className="dropdown-item" href="/MakeAppointment">Sign Up For an Appointment</a>
+            </NavDropdown>
+
+            <NavDropdown name="Groups">
+                <a className="dropdown-item" href="/notfound">View Current Groups</a>
+            </NavDropdown>
+            
+          </ul>
+          <ul className="navbar-nav ml-auto">
+              <NavItem  path="/logout" name="Logout" />            
+          </ul>
+        </div>
+      </nav> 
+                    )
+
+                  }
+                }
+            }
+    
   }
 }
 
