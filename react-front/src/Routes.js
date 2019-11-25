8257a3cfe3f66c5b40cb71f72be447d9b77def7b
ayushg1998 <ayushgupta2013@gmail.com>
2019-11-25 00:57:17 -0600

    Overwrite fixes

--- react-front/src/Routes.js
+++ react-front/src/Routes.js
@@ -16,6 +16,8 @@ import ShareCalendar from './containers/ShareCalendar';
 import CreateGroups from './containers/CreateGroups';
 import ShareGroups from './containers/ShareGroups';
 import UploadGroup from './containers/UploadGroup';
+import UploadUser from './containers/UploadUser';
+import CreateFaculty from './containers/CreateFaculty';
 import EditGroup from './editGroup';
 import ViewGroups from './containers/ViewGroups';
 import ViewAppointments from './containers/viewAppointments';
@@ -42,6 +44,7 @@ const WrappedDashboard = authAwareComponent(Dashboard, true);
 const WrappedCreateStudent = authAwareComponent(CreateStudent, true);
 const WrappedCreateEvent = authAwareComponent(CreateEvent, true);
 const WrappedAppointment = authAwareComponent(Appointment, true);
+const WrappedCreateFaculty = authAwareComponent(CreateFaculty, true);
 const WrappedSignupForAppointment = authAwareComponent(SignupForAppointment, true);
 const WrappedShareCalendar = authAwareComponent(ShareCalendar, true);
 const WrappedCreateGroups = authAwareComponent(CreateGroups, true);
@@ -49,6 +52,7 @@ const WrappedShareGroups = authAwareComponent(ShareGroups, true);
 const WrappedViewGroups = authAwareComponent(ViewGroups, true);
 const WrappedViewAppointments = authAwareComponent(ViewAppointments, true);
 const WrappedUploadGroup = authAwareComponent(UploadGroup, true);
+const WrappedUploadUser = authAwareComponent(UploadUser, true);
 const WrappedEditGroup = authAwareComponent(EditGroup, true);
 const WrappedLogout = authAwareComponent(Logout, true);
 
@@ -87,9 +91,11 @@ export default class Route extends React.Component {
                 <AppliedRoute path="/SignupForAppointment" exact component={WrappedSignupForAppointment} />
                 <AppliedRoute path="/ViewAppointments" exact component={WrappedViewAppointments} />
                 <AppliedRoute path="/CreateGroups" exact component={WrappedCreateGroups} />
+                <AppliedRoute path="/CreateFaculty" exact component={WrappedCreateFaculty} />
                 <AppliedRoute path="/ViewGroups" exact component={WrappedViewGroups} />
                 <AppliedRoute path="/ShareGroups" exact component={WrappedShareGroups} />
                 <AppliedRoute path="/UploadGroup" exact component={WrappedUploadGroup} />
+                <AppliedRoute path="/UploadUser" exact component={WrappedUploadUser} />
                 <AppliedRoute path="/EditGroups" exact component={WrappedEditGroup} />
                 <AppliedRoute path="/ShareCalendar" exact component={WrappedShareCalendar} />
                 <AppliedRoute path="/logout" exact component={WrappedLogout} />
